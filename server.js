b08debbf511bc61ee9fb8fc2542ec3419f787cb7
jlyonas <jlyon@acumensolutions.com>
2018-01-03 18:36:43 -0500

    fix the server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/dist/build.js", (req, res) => {
+	res.sendFile(path.join(__dirname, "dist", "build.js"));
+});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"))
+});
 
 app.listen(port, () => console.log("Listening on port: ", port));
